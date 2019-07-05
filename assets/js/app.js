ee902489e4d231e789e477e96acf64c0f111c66a
Scout <therearespidersinthisemail@gmail.com>
2019-07-04 22:30:09 -0600

    added module for portfolio with image animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,24 @@
 
+var greenMod = document.getElementById("greenMod");
+var span = document.getElementsByClassName("close")[0];
+
+
+function openCodeMod() {
+    greenMod.style.display = "block";
+}
+
+span.onclick = function() {
+    greenMod.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == greenMod) {
+        greenMod.style.display = "none";
+    }
+}
+
+
+
 
 function openNav() {
     document.getElementById("mySideNav").style.width = "250px";
@@ -12,3 +32,4 @@ function closeNav() {
     document.getElementById("btns").style.opacity = "0";
     document.getElementById("btns").style.transition = "0.1s"  
 }
+
