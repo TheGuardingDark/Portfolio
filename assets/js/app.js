93289aa39d34d81e063dca2490c25466cba64971
Scout <therearespidersinthisemail@gmail.com>
2019-07-07 22:06:12 -0600

    added animation to closing tab and modals, starting load up animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,51 +1,51 @@
 
-var greenMod = document.getElementById("greenMod");
-var purpleMod = document.getElementById("purpleMod");
-var blueMod = document.getElementById("blueMod");
 var span = document.getElementsByClassName("close")[0];
-
+var sidenav = document.getElementById("mySideNav");
+var btn = document.getElementById("btns");
 
 
 function openCodeMod() {
-    greenMod.style.display = "block";
-    purpleMod.style.display = "none";
-    blueMod.style.display = "none";
+    $("#greenMod").removeClass("slideOut").addClass("slideIn");
+    $("#blueMod").removeClass("slideIn").addClass("slideOut");
+    $("#purpleMod").removeClass("slideIn").addClass("slideOut");
+    $(".close").addClass("visible");
     closeNav();
 }
 
 function openAboutMod() {
-    purpleMod.style.display = "block";
-    greenMod.style.display = "none";
-    blueMod.style.display = "none";
+    $("#purpleMod").removeClass("slideOut").addClass("slideIn");
+    $("#blueMod").removeClass("slideIn").addClass("slideOut");
+    $("#greenMod").removeClass("slideIn").addClass("slideOut");
     closeNav();
 }
 
 function openContactMod() {
-    blueMod.style.display = "block";
-    greenMod.style.display = "none";
-    purpleMod.style.display = "none";
-    closeNav();
+   $("#blueMod").addClass("slideIn").removeClass("slideOut");
+   $("#purpleMod").removeClass("slideIn").addClass("slideOut");
+   $("#greenMod").removeClass("slideIn").addClass("slideOut");
+   closeNav();
 
 }
 
 function closeMod() {
-    greenMod.style.display = "none";
-    purpleMod.style.display = "none";
-    blueMod.style.display = "none";
+    $("#blueMod").removeClass("slideIn").addClass("slideOut");
+    $("#purpleMod").removeClass("slideIn").addClass("slideOut");
+    $("#greenMod").removeClass("slideIn").addClass("slideOut");
+    closeNav();
 }
 
 
 
 function openNav() {
-    document.getElementById("mySideNav").style.width = "250px";
-    document.getElementById("btns").style.opacity = "1";
-    document.getElementById("btns").style.transition = "4.5s"
+    sidenav.style.width = "250px";
+    btn.style.opacity = "1";
+    btn.style.transition = "4.5s"
 
 }
 
 function closeNav() {
-    document.getElementById("mySideNav").style.width = 0;
-    document.getElementById("btns").style.opacity = "0";
-    document.getElementById("btns").style.transition = "0.1s"  
+    sidenav.style.width = 0;
+    btn.style.opacity = "0";
+    btn.style.transition = "0.1s"  
 }
 
